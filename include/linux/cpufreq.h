394f7a79533e2c9c432c096b08ad130945be925e
djmax81 <djmax81@gmail.com>
2016-04-11 17:40:28 +0530

    Added a few CPU governors [credits obviously to their authors]
    Added a few CPU governors [credits obviously to their authors]
    
    Ported: Few cpu governor to our Exynos 5433 Note 4 not all parameters
    tested so far considered Experimental.
    
    Signed-off-by: djmax81 <djmax81@gmail.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -400,6 +400,15 @@ extern struct cpufreq_governor cpufreq_gov_conservative;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTERACTIVE)
 extern struct cpufreq_governor cpufreq_gov_interactive;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_interactive)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_BIOSHOCK)
+extern struct cpufreq_governor cpufreq_gov_bioshock;
+#define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_bioshock)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTEREXTREM)
+extern struct cpufreq_governor cpufreq_gov_interextrem;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_interextrem)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_LIONHEART)
+extern struct cpufreq_governor cpufreq_gov_lionheart;
+#define CPUFREQ_DEFAULT_GOVERNOR 	(&cpufreq_gov_LIONHEART)
 #endif
 
 
